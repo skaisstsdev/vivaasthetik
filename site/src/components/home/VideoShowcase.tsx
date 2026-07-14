e335a8d1853ed350f310c1d1426e9e112f09a9eb
«skai»sstsdev <skaissts.dev@gmail.com>
2026-07-14 19:08:44 +0500

    chore: temporary swap video1 to video2 for testing performance

--- site/src/components/home/VideoShowcase.tsx
+++ site/src/components/home/VideoShowcase.tsx
@@ -9,8 +9,8 @@ export default function VideoShowcase() {
   return (
     <ScrollExpandMedia
       mediaType="video"
-      mediaSrc="/video1_desktop.mov"
-      mobileMediaSrc="/video1_mobile.mov"
+      mediaSrc="/video2_desktop.mov"
+      mobileMediaSrc="/video2_mobile.mov"
       bgImageSrc="/stranica1.webp"
       title={t('title')}
     />
