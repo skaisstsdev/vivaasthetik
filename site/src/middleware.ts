c8caf87300957978b31da083b9bc29db16e3bcb7
«skai»sstsdev <skaissts.dev@gmail.com>
2026-07-15 09:23:50 +0500

    Fix: Update middleware matcher to catch unlocalized routes like /admin

--- site/src/middleware.ts
+++ site/src/middleware.ts
@@ -4,5 +4,8 @@ import {routing} from './i18n/routing';
 export default createMiddleware(routing);
  
 export const config = {
-  matcher: ['/', '/(de|ru)/:path*']
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
