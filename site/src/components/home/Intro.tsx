e9486880355d7ccc434aff6c49d3598f6e53d4b2
«skai»sstsdev <skaissts.dev@gmail.com>
2026-07-21 20:09:30 +0500

    Increase padding between text and photo on mobile

--- site/src/components/home/Intro.tsx
+++ site/src/components/home/Intro.tsx
@@ -6,7 +6,7 @@ export default function Intro() {
   const t = useTranslations('Intro');
 
   return (
-    <section className="pt-24 pb-8 px-8 bg-white">
+    <section className="pt-24 pb-12 md:pb-8 px-8 bg-white">
       <div className="max-w-4xl mx-auto text-center flex flex-col items-center justify-center">
         <h2 className="text-4xl md:text-5xl font-light text-gray-900 mb-10 leading-snug">
           {t('title')}
